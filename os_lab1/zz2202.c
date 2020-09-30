8d44576fca2741219827306745477db4c8126869
Karen Zhou <zz2202@nyu.edu>
2020-09-30 15:59:24 -0400

    modified zz2202.c againn

--- os_lab1/zz2202.c
+++ os_lab1/zz2202.c
@@ -4,7 +4,8 @@
 #include<sys/types.h> 
 #include<sys/wait.h>
 #include <string.h>
-#define MAXLEN 1000
+#define ARGLEN 1000
+#define PATHLEN 100
 
 int main() {
     while (1) {
@@ -24,7 +25,7 @@ int main() {
             if (pid == 0) { // we enter the child process
                 printf("Child process %d will execute the command %s\n", getpid(), argv);
                 // split the argv string into a list of strings with NULL at the end of the list
-                char ** arg_lst = malloc(sizeof(char) * MAXLEN);
+                char ** arg_lst = malloc(sizeof(char) * ARGLEN);
                 char * token;
                 token = strtok(argv, " ");
                 int i = 0;
@@ -35,10 +36,9 @@ int main() {
                 }
                 arg_lst[i+1] = NULL;
                 // create a path as the first argument
-                char * path = "/bin/";
-                // strcpy(path, "/bin/");
+                char * path[PATHLEN];
+                strcpy(path, "/bin/");
                 strcat(path, arg_lst[0]);
-                printf("path is %s\n", path);
                 if (execve(path, arg_lst, NULL) < 0) {
                     printf("Command Not Found!\n");
                     exit(0);
