0dd6ef3fe587b0dca9f8a2ad420a8d8200024d41
Karen Zhou <zz2202@nyu.edu>
2020-09-30 14:03:12 -0400

    modified zz2202.c

--- os_lab1/zz2202.c
+++ os_lab1/zz2202.c
@@ -7,7 +7,7 @@
 int main() {
     while (1) {
         printf("lab1> ");
-        char * argv[10];
+        char * argv;
         fgets(argv, 500, stdin);
         printf("Parent Process %d\n", getpid());
         if (strcmp(argv, "printid") == 0) {
@@ -21,7 +21,7 @@ int main() {
             if (pid == 0) { // we enter the child process
                 printf("Child process %d will execute the command %s\n", getpid(), argv);
                 char * path = "/bin/";
-                strcat(path, argv[0]);
+                strcat(path, argv);
                 execve(path, argv, NULL);
                 printf("Command Not Found!\n");
                 exit(0);
