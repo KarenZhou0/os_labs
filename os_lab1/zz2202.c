6392ede6e3915acbf1ad4ba9b6ff244175639c56
Karen Zhou <zz2202@nyu.edu>
2020-10-09 21:11:50 -0400

    changes

--- os_lab1/zz2202.c
+++ os_lab1/zz2202.c
@@ -11,7 +11,7 @@
 int main() {
     while (1) {
         printf("lab1> ");
-        char * argv[ARGLEN] = "/bin/";
+        char argv[ARGLEN] = "/bin/";
         fgets(argv, ARGLEN, stdin);
         argv[strlen(argv) - 1] = '\0';
         printf("Parent Process %d\n", getpid());
